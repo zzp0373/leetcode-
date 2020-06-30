1c6d7d486bbb11f74fc18f017023dadbcdaf489a
zhangzhanpeng <zhangzhanpeng@huami.com>
2020-06-30 12:44:56 +0800

    每天一道题

--- 20200628.js
+++ 20200628.js
@@ -5,21 +5,20 @@
 // 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组
 var minSubArrayLen = function (s, nums) {
     let n = nums.length
-    if (n = 0) {
+    if (n === 0) {
         return 
     }
-    var ans = 0
+    var ans = 999
+    console.log(ans,11,n)
     for (var i = 0; i < n ; i ++) {
         var sum = 0
         for (var j = i; j < n; j ++) {
             sum = sum + nums[j]
             if (sum >= s) {
-
                 ans = Math.min(ans, j - i + 1);
             }
         }
     }
-    return ans
+    return ans === 999 ? 0 : ans
 }
-console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
-console.log(222)
+console.log(minSubArrayLen(999, [2,3,1,2,4,3]))
