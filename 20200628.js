ddca8d243cac08634b30671f085f7001302d4b0a
zhangzhanpeng <zhangzhanpeng@huami.com>
2020-06-30 09:52:36 +0800

    zzp 第一次提交

---
+++ 20200628.js
@@ -0,0 +1,24 @@
+// 给定一个含有 n 个正整数的数组和一个正整数 s ，找出该数组中满足其和 ≥ s 的长度最小的连续子数组，并返回其长度。如果不存在符合条件的连续子数组，返回 0。
+
+// 输入: s = 7, nums = [2,3,1,2,4,3]
+// 输出: 2
+// 解释: 子数组 [4,3] 是该条件下的长度最小的连续子数组
+var minSubArrayLen = function (s, nums) {
+    let n = nums.length
+    if (n = 0) {
+        return 
+    }
+    var ans = 0
+    for (var i = 0; i < n ; i ++) {
+        var sum = 0
+        for (var j = i; j < n; j ++) {
+            sum = sum + nums[j]
+            if (sum >= s) {
+
+                ans = Math.min(ans, j - i + 1);
+            }
+        }
+    }
+    return ans
+}
+console.log(minSubArrayLen(7, [2,3,1,2,4,3]))
